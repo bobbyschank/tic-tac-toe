073bd7dc588978d11a93f6c56f73da17abb18564
bobbyschank <robertschank@gmail.com>
2017-03-22 22:29:31 -0600

    lots

--- tictac.js
+++ tictac.js
@@ -1,30 +1,55 @@
-console.log("hello");
-
 let blocks = document.querySelectorAll('td');
+let resetButton = document.querySelector('button');
+let gump = document.querySelector('section');
+let player = "X";
+let counter = 1;
 
-console.log(blocks);
+let rowCount = [0,0,0];
+let colCount = [0,0,0];
+let diag1Count = 0;
+let diag2Count = 0;
 
 blocks.forEach(function(block) {
 	block.addEventListener("click", function(){
-	let classes = block.className;
-	let classesArray = classes.split(' ');
-	console.log("classes: " + classesArray[1]);
-		onBlockClick(block);
+		if (block.textContent === '' ) {
+
+			block.textContent = player;
+
+			let rowMarked = block.getAttribute("data-row");
+			rowCount[rowMarked] += counter;
+			if (Math.abs(rowCount[rowMarked]) >= 3) {alert(player + " PLAYER WINS!");}
+
+			let colMarked = block.getAttribute("data-col");
+			colCount[colMarked] += counter;
+			if (Math.abs(colCount[colMarked]) >= 3) {alert(player + " PLAYER WINS!");}
+
+			if (block.getAttribute("data-diag1")) {
+				diag1Count += counter;
+				if (Math.abs(diag1Count) >= 3) {alert(player + " PLAYER WINS!");}
+			}
+
+			if (block.getAttribute("data-diag2")) {
+			diag2Count += counter;
+			if (Math.abs(diag2Count) >= 3) {alert(player + " PLAYER WINS!");}
+			}
+
+			togglePlayer();
+
+		} else {
+			gump.className = "taken";
+		}
 	});
 });
 
-function onBlockClick(block) {
-	console.log(block);
-	block.textContent = "X";
-	togglePlayer();
-	count(block);
-}
-
 function togglePlayer() {
-
+	player = player === "X" ? "O" : "X";
+	counter = counter * -1;
 }
 
-function count(block) {
-	console.log("count block: " + block);
-	console.log("className: " + block.className);
-}
+resetButton.addEventListener("click", function(){
+	location.reload();
+});
+
+gump.addEventListener("click", function(){
+	gump.className = null;
+});
